e062429f89428f7a339a110a2caa8149ca597ab3
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-04 15:41:02 +0200

    learn and add: If_else.kt

---
+++ src/main/kotlin/If_else.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val phoneLang: String = "uk" // this is language on phone
+
+    if (phoneLang == "uk") {
+        println("Привiт свiт")
+    } else if (phoneLang == "fr") {
+        println("Bonjour")
+    } else {
+        println("Hello world")
+    }
+    if (true) {
+        println("true")
+    }
+    println("The end of program.")
+}
