eecc8e124153fe8f1a3ced4e04192d53f34fc4f3
Dmitry Zadnepryansky <d.zadnepryansky@gmail.com>
2023-01-04 15:21:48 +0200

    learn and add: mathOperators.kt

---
+++ src/main/kotlin/mathOperators.kt
@@ -0,0 +1,22 @@
+//fun main(){
+//    // + - * / %
+//}
+
+fun main() {
+    val x: Int = 3
+    val y: Int = 2
+    println(x + y)
+    println(x - y)
+    println(x * y)
+
+    val xAsDouble : Double = x.toDouble()
+    val yAsDouble: Double = y.toDouble()
+    val result: Double = xAsDouble / yAsDouble
+    println(result)
+
+    println(25 % 10)
+
+    val myResult: Int = 3 - 2 * 2 / 1
+    println(myResult)
+
+}
